9f23caf6dbdf331bc208811b23f0c24653982807
Hank860502 <hank860502@gmail.com>
2018-02-03 15:56:01 -0500

    bug on the counter

--- GuessingGame.java
+++ GuessingGame.java
@@ -11,44 +11,93 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class GuessingGame {
-	 private static Scanner scanner = new Scanner( System.in );
+	private static Scanner scanner = new Scanner( System.in );
 	// fill in code here
 	// define data members
-	ArrayList<String> al= new ArrayList<String>();
 	private int count;
-
-	Random rand = new Random();
-	int firstGuess = rand.nextInt(9999) + 1000;
-	// get a random number between 1000 to 9999
+	private ArrayList<Integer> al;
+	private ArrayList<Integer> store;
+	private boolean foundAllNumbers;
 
 	public GuessingGame ( ) {
 		// fill in code here
 		// initialization
-
+		this.count = 0;
+		this.foundAllNumbers = false;
+		this.al = new ArrayList<Integer>();
+		// al.add(0000); can't test on this because 0000 = 0
+	  al.add(1111);
+	  al.add(2222);
+	  al.add(3333);
+	  al.add(4444);
+	  al.add(5555);
+	  al.add(6666);
+	  al.add(7777);
+	  al.add(8888);
+	  al.add(9999);
+		this.store = new ArrayList<Integer>(); // store all the matching nums
 
 	}
 
 	public int myGuessIs() {
 		// fill in code here
-		if(this.count == 0){
-			return firstGuess;
-		} else if(al.size()==0){
+		// return -1 if no valid guess
+		// return the next guess value
+		// add count 1 on the totalNumguess
+		if(this.store.size() == 4){
+			this.foundAllNumbers = true;
+			// list out all the possible numbers that contains these four numbers but 0 could not be the first digit
+			int test = 0;
+			while (store.size() > test) {
+	 			System.out.println(store.get(test));
+         test++;
+      }
+			this.count++;
+      return this.store.get(1);
+		} else if (this.count<4){
+			System.out.println("RANNNNN");
+			this.count++;
+			return this.al.get(0);
+		} else if (this.al.size()==0){ // BUUUGGGG
 			return -1;
-		} else{
-			return al.get(rand.nextInt(al.size()));
-			// getting a random number from the remain arraylist as the next guess
 		}
+		return this.al.get(0);
+
 	}
 	
 	public int totalNumGuesses() {
-		this.count += 1;
 		// fill in code here
 		// this should return the total number of guesses taken
+		return this.count;
 	}
  
 	public void updateMyGuess(int nmatches) {
 		// fill in code here
 		// update the guess based on the number of matching digits claimed by the user
+		if (this.foundAllNumbers == false) {
+			if (nmatches == 0) {
+				this.al.remove(0); // if no number matches, delete that value from al and move on to next guess
+				myGuessIs();
+			} else if (this.al.size() == 0){
+				for (int i = (4-store.size()); i>0; i--) {
+					this.store.add(0); // if run through the 9 pairs of numbers and still couldn't get which 4 numbers is in size, add the remain spot with 0
+				} 
+				myGuessIs();
+			} else if (nmatches>0) {
+				for (int i=nmatches; i>0; i--) {
+					this.store.add(this.count);
+					System.out.println(this.store.size() + "sizeeee" + "Count is at " + this.count);
+				}
+				this.al.remove(0);
+				myGuessIs();
+			}
+		} else if (this.foundAllNumbers == true){
+			// if (nmatches == 0) {
+
+			// }
+		}
+			// return al.get(rand.nextInt(al.size()));
+			// getting a random number from the remain arraylist as the next guess
 	}
 	
 	// fill in code here (optional)
@@ -70,6 +119,8 @@ public class GuessingGame {
 		
 		do {
 			myguess = gamer.myGuessIs();
+			System.out.println("the count is at" + gamer.count);
+
 			if (myguess == -1) {
 				System.out.println("I don't think your number exists!");
 				System.exit(0);
