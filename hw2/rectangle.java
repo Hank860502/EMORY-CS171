52978ed6dc79a490ebbf76d9bfa2775d07a433c8
Hank860502 <hank860502@gmail.com>
2018-02-10 12:52:04 -0500

    hw 2 initiated

---
+++ hw2/rectangle.java
@@ -0,0 +1,32 @@
+//A class for a rectangle.
+
+public class Rectangle{//save as Rectangle.java.
+
+	 // private instance variable, not accessible from outside this class
+
+
+
+	private double L;//the length of the rectangle
+   private double H; //the height of the rectangle
+   private double x;// The x coordinate of the  bottom left corner of the rectangle
+   private double y; // the y coordinate of the bottom left corner of the rectangle
+
+
+   //You should write a basic constructor that creates a rectangle with side lengths 1 whose bottom left corner is at (0.0,0.0)
+
+
+   //Now write a constructor that takes all four inputs, named Ell, Eich, Ex, and Why for L, H, x, and y respectively. 
+
+   //Write a method called getLenght which returns the length.
+
+   //Write a method called getHeight which returns the height. 
+
+   //Write a method called setLength which takes as input a double called Ell which sets the length of the rectangle.
+
+   //write a method called setHeight which takes as input a double called Eich which sets the height of the rectangle.
+
+   //write a method called perimeter which computes the perimeter of the rectangle. 
+
+   //write a method called area which computes the area of the rectangle. 
+
+}
